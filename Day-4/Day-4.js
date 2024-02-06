38713173ac89efc77e7ec424dd0ec7be05952e18
akashsonune <akashsonune7720@gmail.com>
2024-02-06 09:19:57 +0530

    updates

---
+++ Day-4/Day-4.js
@@ -0,0 +1,19 @@
+
+// for ( var i =2;  i<=10 ; i ++){
+//     if( i % 2 +2){
+      
+//     }
+// }
+
+// console.log(i)
+
+
+
+// add numbers
+
+for (var i=1 ; i >=100 ; i++){
+    if (i % 2 !=0){
+
+    }
+}
+console.log(i)
